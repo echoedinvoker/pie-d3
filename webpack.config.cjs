115a9558478d2606143659698070b1571e730729
Matt Chang <echoedinvoker@gmail.com>
2024-03-06 14:15:16 +0800

    first commit

---
+++ webpack.config.cjs
@@ -0,0 +1,33 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    compress: true,
+    port: 9000, // 您可以選擇任何想要的端口
+    hot: true, // 啟用熱重載
+  },
+  externals: {
+    d3: 'd3',
+  },
+};
+
